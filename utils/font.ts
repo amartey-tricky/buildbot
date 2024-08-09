904fc65d0ae23f557e4cb8e0a373c178913ab3a2
Tricky <trickya23@outlook.com>
2024-08-09 00:00:47 +0000

    fix security

--- utils/font.ts
+++ utils/font.ts
@@ -1,12 +1,25 @@
-import { Josefin_Sans, Platypi } from "next/font/google";
+import { Josefin_Sans, Platypi, Timmana, Bitter } from "next/font/google";
 
 export const josefin = Josefin_Sans({
   subsets: ["latin"],
   variable: "--font-josefin",
   weight: ["400", "600", "700"],
+  fallback: ["sans-serif"],
 });
 
 export const platypi = Platypi({
   weight: ["400", "600", "700"],
   subsets: ["latin"],
+  fallback: ["monserrat", "sans-serif"],
+});
+
+export const timmana = Timmana({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const bitter = Bitter({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+  fallback: ["sans-serif"],
 });
